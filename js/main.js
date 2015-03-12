9e7ee4b5ad9afb36133117addd261da5357e314b
Alex North <alex@alexn.id.au>
2015-03-13 10:51:09 +1100

    Inserted Firepad for an editor

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
+(function () {
 
-var firebase = new Firebase("https://wemail.firebaseio.com/");
+  var firebase = new Firebase("https://wemail.firebaseio.com/");
+
+  var codeMirror = CodeMirror(document.getElementById('firepad'), {lineWrapping: true});
+  var firepad = Firepad.fromCodeMirror(firebase, codeMirror, {
+    //userId: userId,
+    richTextShortcuts: true,
+    richTextToolbar: true,
+    defaultText: 'Hello, World!'
+  });
+
+
+  firepad.on('ready', function() { console.log("Firepad ready"); });
+  firepad.on('synced', function() { console.log("Firepad synced"); });
+
+})();
+
