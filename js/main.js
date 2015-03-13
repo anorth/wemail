420667baeac5e076e0e6ec1621b23d6267175c34
Alex North <alex@alexn.id.au>
2015-03-13 11:49:36 +1100

    Save pads individually

--- js/main.js
+++ js/main.js
@@ -1,56 +1,77 @@
 (function () {
-
   var firebase = new Firebase("https://wemail.firebaseio.com/");
+  var firepad;
+  bind();
 
   firebase.onAuth(function(authData) {
     if (authData) {
       //console.log(authData);
-      document.getElementById("signedin").innerText = authData.google.displayName +
-        ' <' + authData.google.email + '>';
       console.log('Logged in ' + authData.uid + ' ' + authData.google.email);
+      document.getElementById("signedin").innerText = authData.google.displayName +
+      ' <' + authData.google.email + '>';
       // Note: user data is not (yet) persisted to firebase
+
+      var padId = window.location.hash.slice(1) || undefined;
+      firepad = initFirepad(authData.uid, padId);
     } else {
       document.getElementById("signedin").innerText = "No";
       console.log("User is logged out");
     }
   });
 
-  var codeMirror = CodeMirror(document.getElementById('firepad'), {lineWrapping: true});
-  var firepad = Firepad.fromCodeMirror(firebase, codeMirror, {
-    //userId: userId,
-    richTextShortcuts: true,
-    richTextToolbar: true,
-    defaultText: 'Hello, World!'
-  });
 
 
-  firepad.on('ready', function() { console.log("Firepad ready"); });
-  firepad.on('synced', function() { console.log("Firepad synced"); });
 
-  document.getElementById("signin").onclick = function() {
-    firebase.authWithOAuthPopup("google", function(error, authData) {
-      if (error) {
-        if (error.code === "TRANSPORT_UNAVAILABLE") {
-          // fall-back to browser redirects, and pick up the session
-          // automatically when we come back to the origin page
-          firebase.authWithOAuthRedirect("google", function(error) {
+  function bind() {
+    document.getElementById("signin").onclick = function() {
+      firebase.authWithOAuthPopup("google", function(error, authData) {
+        if (error) {
+          if (error.code === "TRANSPORT_UNAVAILABLE") {
+            // fall-back to browser redirects, and pick up the session
+            // automatically when we come back to the origin page
+            firebase.authWithOAuthRedirect("google", function(error) {
+              console.log("Login Failed!", error);
+            });
+          } else {
             console.log("Login Failed!", error);
-          });
+          }
         } else {
-          console.log("Login Failed!", error);
+          console.log("Authenticated successfully with payload:", authData);
         }
-      } else {
-        console.log("Authenticated successfully with payload:", authData);
-      }
-    }, {
-      scope: "email"
+      }, {
+        scope: "email"
+      });
+    };
+
+    document.getElementById("signout").onclick = function() {
+      firebase.unauth();
+      window.location.reload();
+    };
+  }
+
+  function initFirepad(userId, padId) {
+    var pads = firebase.child('pads');
+    var padRef;
+    if (!!padId) {
+      padRef = pads.child(padId);
+    } else {
+      padRef = pads.push();
+      padId = padRef.key();
+    }
+
+    var codeMirror = CodeMirror(document.getElementById('firepad'), {lineWrapping: true});
+    var firepad = Firepad.fromCodeMirror(padRef, codeMirror, {
+      userId: userId,
+      richTextShortcuts: true,
+      richTextToolbar: true,
+      defaultText: 'Hello, World!'
     });
-  };
 
-  document.getElementById("signout").onclick = function() {
-    firebase.unauth();
-    window.location.reload();
-  };
-
+    firepad.on('ready', function() { console.log("Firepad ready"); });
+    firepad.on('synced', function() { console.log("Firepad synced"); });
+    document.getElementById('padid').innerText = padId;
+    window.location.hash = padId;
+    return firepad;
+  }
 })();
 
