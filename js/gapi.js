b738bc2f0a88639ead8244c68fafbd5fb56c93c3
Adam Schuck <aschuck@gmail.com>
2015-03-19 12:21:02 +1100

    Remove deps on unused gmail lib.

--- js/gapi.js
+++ js/gapi.js
@@ -2,45 +2,6 @@
  * Google API-related code, e.g. to invoke the GMail api.
  */
 
-function handleGapiLoad() {
-  console.log("Google API loaded.");
-
-  WEMAIL_CLIENT_ID = '638270974877-c2ss7kkkofab78g9cirjdm5ubgpfoegv.apps.googleusercontent.com';
-
-  // doc: https://developers.google.com/api-client-library/javascript/reference/referencedocs#gapiauthinit
-  // This gives "origin_mismatch" error in the oauth flow.
-  /*
-  gapi.auth.authorize({
-    client_id: WEMAIL_CLIENT_ID,
-    immediate: false,  // ideally true, to have this occur in background
-    scope: 'email https://www.googleapis.com/auth/gmail.compose'
-  }, function(token) {
-    console.log(token);
-  });
-  */
-
-  gapi.client.load('gmail', 'v1').then(initFirebase);  // TODO(adam): confirm best version.
-}
-
-/**
- * Send Message.
- *
- * @param  {String} userId User's email address. The special value 'me'
- * can be used to indicate the authenticated user.
- * @param  {String} email RFC 5322 formatted String.
- * @param  {Function} callback Function to call when the request is complete.
- */
-function sendMessage(userId, email, callback) {
-  var base64EncodedEmail = btoa(email);
-  var request = gapi.client.gmail.users.messages.send({
-    'userId': userId,
-    'message': {
-      'raw': base64EncodedEmail
-    }
-  });
-  request.execute(callback);
-}
-
 
 /**
  * Sends an email via the REST API.
@@ -57,6 +18,8 @@ function sendMessage(userId, email, callback) {
 function sendEmail(googleAccessToken, toEmail, subject, body, callback) {
   // NOTE(adam): to make this work, Gmail API needed to be enabled within the developer console:
   // https://console.developers.google.com/project/wemail-dev/apiui/apiview/gmail/usage
+  // TODO(adam): if not using gapi more extensively, don't include google's JS,
+  // use another lib for XHR.
   gapi.client.request({
     path: 'https://content.googleapis.com/gmail/v1/users/me/messages/send',
     method: 'POST',
@@ -72,6 +35,7 @@ function sendEmail(googleAccessToken, toEmail, subject, body, callback) {
     }
   }).execute(callback);
 }
+
 
 /**
  * Sends an email invite to a collaborator to join the draft.
