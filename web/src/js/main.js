ad7ece954ec76054a6036c96a9145a356d953fce
Adam Schuck <aschuck@gmail.com>
2015-04-13 22:27:34 +1000

    Use existing c2a on landing page.

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -7,7 +7,7 @@
     var redirectUri = document.location.origin + '/draft';
     var siginInUrl = window.gmail.getSignInUrl(redirectUri);
 
-    $('#signin').attr('href', siginInUrl);
+    $('.actions .auth').attr('href', siginInUrl);
   }
 
   $(document).ready(main);
